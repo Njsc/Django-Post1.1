fe82b8cc36196a3033677318e63465df6c2f6e20
root <root1111>
2016-06-01 23:38:22 +0800

    add userconfirmation

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
@@ -9,4 +8,5 @@ urlpatterns = [
     url(r'^draft/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
+
 ]
